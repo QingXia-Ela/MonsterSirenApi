4545c71c45a8d11baeca0c96787eb8309bbfaad8
QingXia-Ela <Elahere@163.com>
2023-03-14 22:33:08 +0800

    fix(docs): word incorrent problem
    feat: add somke test

---
+++ test/smoke.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+import assert from "assert";
+
+function sum(...rest) {
+  var sum = 0
+  for (var one of rest) {
+    sum += one
+  }
+  return sum
+}
+
+describe("大的组1测试", () => {
+  describe("小的组1测试", () => {
+    it("sum() 0", () => {
+      assert.strictEqual(sum(), 0)//测试用例
+    })
+  })
+  describe("小的组2测试", () => {
+    it("sum(1) 1", () => {
+      assert.strictEqual(sum(1), 2)
+    })
+    it("sum(1,2) 3", () => {
+      assert.strictEqual(sum(1, 2), 3)
+    })
+  })
+})
