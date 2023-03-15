a581a62dcaab9b8f4bbafc6487f7b61e3192cb6e
QingXia-Ela <Elahere@163.com>
2023-03-15 14:44:07 +0800

    feat: add module exports
    chore: change run script

---
+++ packages/modules/album.ts
@@ -0,0 +1,5 @@
+// interface
+
+export const getAlbums = function (qurey: any, request: any) {
+
+}
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,7 +1,30 @@
 import express from "express";
+import * as AllModules from '../modules/AllModules'
 
 function createServer() {
   const app = express()
+
+  const CORS_ALLOW_ORIGIN = false
+
+  console.log(AllModules);
+
+  /**
+   * CORS & Preflight request
+  */
+  app.use((req, res, next) => {
+    if (req.path !== '/' && !req.path.includes('.')) {
+      res.set({
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Origin':
+          CORS_ALLOW_ORIGIN || req.headers.origin || '*',
+        'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
+        'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+        'Content-Type': 'application/json; charset=utf-8',
+      })
+    }
+    req.method === 'OPTIONS' ? res.status(204).end() : next()
+  })
+
   return app
 }
 
